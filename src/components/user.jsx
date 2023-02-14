dee88f5832af6297e91460a8a3b283358ef1f337
FunChosa <gilazieva.00@mail.ru>
2023-02-14 14:34:27 +0300

    1434

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -3,113 +3,77 @@ import api from "../api";
 
 const Users = () => {
   const [users, setUsers] = useState(api.users.fetchAll());
-  const titles = [
-    "Имя",
-    "Качества",
-    "Профессия",
-    "Встретился,раз",
-    "Оценка",
-    "",
-  ];
+
   const handleDelete = (userId) => {
-    setUsers((prevState) => prevState.filter((item) => item._id !== userId));
+    setUsers(users.filter((user) => user._id !== userId));
   };
 
   const renderPhrase = (number) => {
-    switch (number) {
-      case 0:
-        return "Никто с тобой не тусанет";
-      case 2:
-      case 3:
-      case 4:
-        return `${number} человека тусанут с тобой сегодня`;
-      default:
-        return `${number} человек тусанет с тобой сегодня`;
-    }
+    const lastOne = Number(number.toString().slice(-1));
+    if (number > 4 && number < 15) return "человек тусанет";
+    if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
+    if (lastOne === 1) return "человек тусанет";
+    return "человек тусанет";
   };
 
-  const renderTitles = () => {
-    //рисуем шапку таблицы
-    return (
-      titles.length !== 0 &&
-      titles.map((title) => (
-        <th key={title} scope="col">
-          {title}
-        </th>
-      ))
-    );
-  };
+  return (
+    <>
+      <h2>
+        <span
+          className={"badge " + (users.length > 0 ? "bg-primary" : "bg-danger")}
+        >
+          {users.length > 0
+            ? `${
+                users.length + " " + renderPhrase(users.length)
+              } с тобой сегодня`
+            : "Никто с тобой не тусанет"}
+        </span>
+      </h2>
 
-  const getBadgeClasses = () => {
-    let classes = "badge ";
-    classes += users.length === 0 ? "bg-danger" : "bg-primary";
-    return classes;
-  };
-
-  const getLabel = () => {
-    return (
-      <>
-        <h2>
-          <span className={getBadgeClasses()}>
-            {renderPhrase(users.length)}
-          </span>
-        </h2>
-      </>
-    );
-  };
-
-  const renderQualities = (user) => {
-    const classes = "p-1 m-1 badge bg-";
-    return user.qualities.map(
-      (
-        element //ругается на отсутствие ключа. пустой тег без ключа низя
-      ) => (
-        <React.Fragment key={element._id}>
-          <td className={classes + element.color}>{element.name}</td>
-        </React.Fragment>
-      )
-    );
-  };
-
-  const renderUsers = () => {
-    return (
-      users.length !== 0 &&
-      users.map((user) => (
-        <tr key={user._id}>
-          <td>{user.name}</td>
-          {/* <React.Fragment key={user._id}> */}
-          {renderQualities(user)}
-          {/* </React.Fragment> */}
-          <td>{user.profession.name}</td>
-          <td>{user.completedMeetings}</td>
-          <td>{user.rate}/5</td>
-          <td>
-            <button
-              className="btn btn-danger btn-sm m-1"
-              onClick={() => handleDelete(user._id)}
-            >
-              delete
-            </button>
-          </td>
-        </tr>
-      ))
-    );
-  };
-
-  if (users.length !== 0) {
-    return (
-      <>
-        {getLabel()}
+      {users.length > 0 && (
         <table className="table">
           <thead>
-            <tr>{renderTitles()}</tr>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th />
+            </tr>
           </thead>
-          <tbody>{renderUsers()}</tbody>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>
+                  {user.qualities.map((item) => (
+                    <span
+                      className={"badge m-1 bg-" + item.color}
+                      key={item._id}
+                    >
+                      {item.name}
+                    </span>
+                  ))}
+                </td>
+                <td>{user.profession.name}</td>
+                <td>{user.completedMeetings}</td>
+                <td>{user.rate} /5</td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn btn-danger"
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-      </>
-    );
-  }
-  return getLabel();
+      )}
+    </>
+  );
 };
 
 export default Users;
