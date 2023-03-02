f14a7104adbfc268679873cf586e51d3c6d9aeb7
FunChosa <gilazieva.00@mail.ru>
2023-03-02 14:13:10 +0300

    1413

--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable indent */
 import React from "react";
 import PropTypes from "prop-types";
+
 const GroupList = ({
     items,
     valueProperty,
@@ -8,39 +9,43 @@ const GroupList = ({
     onItemSelect,
     selectedItem
 }) => {
+    if (!Array.isArray(items)) {
+        return (
+            <ul className="list-group">
+                {Object.keys(items).map((item) => (
+                    <li
+                        key={items[item][valueProperty]}
+                        className={
+                            "list-group-item" +
+                            (items[item] === selectedItem ? " active" : "")
+                        }
+                        onClick={() => onItemSelect(items[item])}
+                        role="button"
+                    >
+                        {items[item][contentProperty]}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
     return (
         <ul className="list-group">
-            {Array.isArray(items)
-                ? items.map((item) => (
-                      <li
-                          key={item[valueProperty]}
-                          className={
-                              "list-group-item" +
-                              (item === selectedItem ? " active" : "")
-                          }
-                          onClick={() => onItemSelect(item)}
-                          role="button"
-                      >
-                          {item[contentProperty]}
-                      </li>
-                  ))
-                : Object.keys(items).map((item) => (
-                      <li
-                          key={items[item][valueProperty]}
-                          className={
-                              "list-group-item" +
-                              (items[item] === selectedItem ? " active" : "")
-                          }
-                          onClick={() => onItemSelect(items[item])}
-                          role="button"
-                      >
-                          {items[item][contentProperty]}
-                      </li>
-                  ))}
+            {items.map((item) => (
+                <li
+                    key={item[valueProperty]}
+                    className={
+                        "list-group-item" +
+                        (item === selectedItem ? " active" : "")
+                    }
+                    onClick={() => onItemSelect(item)}
+                    role="button"
+                >
+                    {item[contentProperty]}
+                </li>
+            ))}
         </ul>
     );
 };
-
 GroupList.defaultProps = {
     valueProperty: "_id",
     contentProperty: "name"
@@ -52,4 +57,5 @@ GroupList.propTypes = {
     onItemSelect: PropTypes.func,
     selectedItem: PropTypes.object
 };
+
 export default GroupList;
--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,96 +1,126 @@
+/* eslint-disable indent */
+
 import React, { useState, useEffect } from "react";
 import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
 import PropTypes from "prop-types";
 import GroupList from "./groupList";
-import User from "./user";
+import _ from "lodash";
 import api from "../api";
 import SearchStatus from "./searchStatus";
-const Users = ({ users: allUsers, ...rest }) => {
+import UsersTable from "./usersTable";
+const Users = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [professions, setProfessions] = useState();
     const [selectedProf, setSelectedProf] = useState();
-    const pageSize = 2;
-    const handlePageChange = (pageIndex) => {
-        setCurrentPage(pageIndex);
+    const [sortBy, setSortBy] = useState({ path: "name", order: "asc" });
+    const pageSize = 8;
+
+    const [users, setUsers] = useState();
+    useEffect(() => {
+        api.users.fetchAll().then((data) => setUsers(data));
+    }, []);
+    const handleDelete = (userId) => {
+        setUsers(users.filter((user) => user._id !== userId));
+    };
+    const handleToggleBookMark = (id) => {
+        setUsers(
+            users.map((user) => {
+                if (user._id === id) {
+                    return { ...user, bookmark: !user.bookmark };
+                }
+                return user;
+            })
+        );
+        console.log(id);
     };
 
     useEffect(() => {
         api.professions.fetchAll().then((data) => setProfessions(data));
     }, []);
+
     useEffect(() => {
         setCurrentPage(1);
     }, [selectedProf]);
+
     const handleProfessionSelect = (item) => {
         setSelectedProf(item);
     };
 
-    const filteredUsers = selectedProf
-        ? allUsers.filter((user) => user.profession === selectedProf)
-        : allUsers;
-    const count = filteredUsers.length;
-
-    const userCrop = paginate(filteredUsers, currentPage, pageSize);
-
-    const clearFilter = () => {
-        setSelectedProf();
+    const handlePageChange = (pageIndex) => {
+        setCurrentPage(pageIndex);
     };
 
-    return (
-        <div className="d-flex">
-            {professions && (
-                <div className="d-flex flex-column flex-shrink-0 p-3">
-                    <GroupList
-                        selectedItem={selectedProf}
-                        items={professions}
-                        onItemSelect={handleProfessionSelect}
-                    />
-                    <button
-                        className="btn btn-secondary mt-2"
-                        onClick={clearFilter}
-                    >
-                        {" "}
-                        Очистить{" "}
-                    </button>
-                </div>
-            )}
-            <div className="d-flex flex-column">
-                <SearchStatus length={count} />
+    const handleSort = (item) => {
+        setSortBy(item);
+    };
 
-                {count > 0 && (
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Имя</th>
-                                <th scope="col">Качества</th>
-                                <th scope="col">Профессия</th>
-                                <th scope="col">Встретился, раз</th>
-                                <th scope="col">Оценка</th>
-                                <th scope="col">Избранное</th>
-                                <th />
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {userCrop.map((user) => (
-                                <User key={user._id} {...rest} {...user} />
-                            ))}
-                        </tbody>
-                    </table>
+    if (users) {
+        const filteredUsers = selectedProf
+            ? users.filter(
+                  (user) =>
+                      JSON.stringify(user.profession) ===
+                      JSON.stringify(selectedProf)
+              )
+            : users;
+
+        const count = filteredUsers.length;
+        const sortedUsers = _.orderBy(
+            filteredUsers,
+            [sortBy.path],
+            [sortBy.order]
+        );
+        const userCrop = paginate(sortedUsers, currentPage, pageSize);
+
+        const clearFilter = () => {
+            setSelectedProf();
+        };
+
+        return (
+            <div className="d-flex">
+                {professions && (
+                    <div className="d-flex flex-column flex-shrink-0 p-3">
+                        <GroupList
+                            selectedItem={selectedProf}
+                            items={professions}
+                            onItemSelect={handleProfessionSelect}
+                        />
+                        <button
+                            className="btn btn-secondary mt-2"
+                            onClick={clearFilter}
+                        >
+                            {" "}
+                            Очистить{" "}
+                        </button>
+                    </div>
                 )}
-                <div className="d-flex justify-content-center">
-                    <Pagination
-                        itemsCount={count}
-                        pageSize={pageSize}
-                        currentPage={currentPage}
-                        onPageChange={handlePageChange}
-                    />
+                <div className="d-flex flex-column">
+                    <SearchStatus length={count} />
+
+                    {count > 0 && (
+                        <UsersTable
+                            users={userCrop}
+                            onSort={handleSort}
+                            selectedSort={sortBy}
+                            onDelete={handleDelete}
+                            onToggleBookMark={handleToggleBookMark}
+                        />
+                    )}
+                    <div className="d-flex justify-content-center">
+                        <Pagination
+                            itemsCount={count}
+                            pageSize={pageSize}
+                            currentPage={currentPage}
+                            onPageChange={handlePageChange}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+    return "loading...";
 };
 Users.propTypes = {
-    users: PropTypes.array.isRequired,
-    length: PropTypes.number
+    users: PropTypes.array
 };
 export default Users;
